99e1b5c870e5487d1b965934645a03841aeda5bd
nurulhassan <nurulhassan2000@gmail.com>
2020-10-01 19:38:30 +0530

    link added

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ class App extends Component {
     };
   }
   
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}));
+  }
+
   render(){
     return(
       <div className="App">
