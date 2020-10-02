a43d48b7b1fccf1b6a865211f4a48952c3431473
nurulhassan <nurulhassan2000@gmail.com>
2020-10-02 16:49:21 +0530

    card css added

--- src/App.js
+++ src/App.js
@@ -27,18 +27,19 @@ class App extends Component {
     .then(response => response.json())
     .then(users => this.setState({monsters:users}));
   }
-
+  
   render(){
     return(
       <div className="App">
-        <CardList name='Nurul'>
-        <h1>Nurul</h1>
-        </CardList>
+      <CardList name='Nurul'>
+      <h1>Nurul</h1>
       {this.state.monsters.map(monster => (
-      <h1 key={monster.id}>{monster.name}</h1>
-    ))}
-  </div>
-    )}
-}
-
-export default App;
+        <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+        </CardList>
+        </div>
+        )}
+      }
+      
+      export default App;
+      
