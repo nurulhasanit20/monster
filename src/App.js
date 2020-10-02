e848fddaeff95d3c9a15d165b88e162d6ac9a006
nurulhassan <nurulhassan2000@gmail.com>
2020-10-02 17:07:26 +0530

    card component added with css

--- src/App.js
+++ src/App.js
@@ -31,15 +31,10 @@ class App extends Component {
   render(){
     return(
       <div className="App">
-      <CardList name='Nurul'>
-      <h1>Nurul</h1>
-      {this.state.monsters.map(monster => (
-        <h1 key={monster.id}>{monster.name}</h1>
-        ))}
-        </CardList>
-        </div>
-        )}
-      }
+      <CardList monsters={this.state.monsters} />
+      </div>
+    )
+      }}
       
       export default App;
       
